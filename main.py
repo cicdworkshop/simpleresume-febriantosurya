d2cf96afa02857b5b3103dba4b15c26f15614deb
febriantosurya <febriantosurya5@gmail.com>
2025-02-22 14:51:39 +0700

    fix: fix issue

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ APP                 = Flask(__name__)
 APP.config["DEBUG"] = True
 HOST                = "0.0.0.0"
 PORT                = 5000
-DATABASE_USER       = "admin"
-DATABASE_PASSWORD   = "admin12345678"
+
+#CSRF
+csrf = CSRFProtect()
+csrf.init_app(APP)
 
 
 @APP.route("/")
